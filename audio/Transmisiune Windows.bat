becebef171234b9321cc6c4830d2650144c6f735
GigaCloud <radu.craciun420@gmail.com>
2021-09-17 17:52:28 +0300

    Modificat transmisiunea de la Windows, merge pe default device din sound mixer

--- audio/Transmisiune Windows.bat
+++ audio/Transmisiune Windows.bat
@@ -1,3 +1,3 @@
 echo "EU EMIT SUNET DE SISTEM"
 
-F:\gstreamer\1.0\msvc_x86_64\bin\gst-launch-1.0 wasapisrc device="\{0.0.1.00000000\}.\{372561ac-a454-4ddd-be3d-3f5d688eadb8\}" ! audioconvert ! audio/x-raw, rate=48000, channels = 1, format=S16LE ! udpsink host=10.8.0.1 port=5000
+F:\gstreamer\1.0\msvc_x86_64\bin\gst-launch-1.0 wasapisrc ! audioconvert ! audio/x-raw, rate=48000, channels = 1, format=S16LE ! udpsink host=10.8.0.1 port=5000
